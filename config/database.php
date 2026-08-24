b5a0c5541a984d4e9172bfdbde59169cba3d52a8
Dulan Vishwajith <dulanvishwajithgunarathna@gmail.com>
2026-08-24 22:20:31 +0530

    Database connection Improve  and packages improve

--- config/database.php
+++ config/database.php
@@ -1,21 +1,33 @@
 <?php
 
 $host = "localhost";
+$dbname = "veyro";
 $username = "root";
 $password = "";
-$database = "veyro";
 
-$conn = new mysqli(
-    $host,
-    $username,
-    $password,
-    $database
-);
+try {
 
-if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password
+    );
+
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
+    $pdo->setAttribute(
+        PDO::ATTR_DEFAULT_FETCH_MODE,
+        PDO::FETCH_ASSOC
+    );
+
+} catch (PDOException $e) {
+
+    die(
+        "Database connection failed: " .
+        $e->getMessage()
+    );
+
 }
-
-$conn->set_charset("utf8mb4");
-
-?>
