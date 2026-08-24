157fbc8a139b857be823cea1298922ad35dea482
Dulan Vishwajith <dulanvishwajithgunarathna@gmail.com>
2026-08-24 17:36:40 +0530

    Edit index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="public/css/hero.css">
     <link rel="stylesheet" href="public/css/quick-features.css">
     <link rel="stylesheet" href="public/css/services.css">
+    <link rel="stylesheet" href="public/css/packages.css">
     <link rel="stylesheet" href="public/css/cta.css">
     <link rel="stylesheet" href="includes/css/footer.css">
 </head>
@@ -21,6 +22,7 @@
     <?php include 'public/hero.php'; ?>
     <?php include 'public/quick-features.php'; ?>
     <?php include 'public/services.php'; ?>
+    <?php include 'public/packages.php'; ?>
     <?php include 'public/cta.php'; ?>
     <?php include 'includes/footer.php'; ?>
 
