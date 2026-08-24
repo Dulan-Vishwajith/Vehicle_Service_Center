221e4382aa8f22d267e31fee60a6e9c8939896e6
Dulan Vishwajith <dulanvishwajithgunarathna@gmail.com>
2026-08-24 21:40:07 +0530

    Improve stylish

--- public/cta.php
+++ public/cta.php
@@ -25,7 +25,7 @@
 
         <a
             href="book-appointment.php"
-            class="cta-btn btn-light btn-large"
+            class="btn btn-light btn-large"
         >
             Book Appointment <span>→</span>
         </a>
